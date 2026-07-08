917c53be51903dafe770e3583a4cdfd2eac177d4
jimlorm <jimlormmartinez@gmail.com>
2026-07-08 19:52:37 +0800

    feat: initialize user class

--- User.java
+++ User.java
@@ -1,3 +1,22 @@
 public class User {
-    
+    private String username;
+    private Library myLibrary;
+
+    public User(String username) {
+        this.username = username;
+        this.myLibrary = new Library();
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public Library getLibrary() {
+        return myLibrary;
+    }
+
+    public void setUsername(String name) {
+        username = name;
+    }
+
 }
