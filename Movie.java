df5629687d3ba8c6b2f5e19f4cba090e62d1d4c5
jimlorm <jimlormmartinez@gmail.com>
2026-07-08 19:54:42 +0800

    feat: initialize `Movie` class

--- Movie.java
+++ Movie.java
@@ -1,3 +1,14 @@
 public class Movie extends MediaEntry {
-    
+    private String director;
+    private int runtimeMinutes;
+
+    public Movie(String title, String genre, String status, String director, int runtimeMinutes) {
+        super(title, genre, status);
+        this.director = director;
+        this.runtimeMinutes = runtimeMinutes;
+    }
+
+    public void displayDetails() {
+
+    }
 }
