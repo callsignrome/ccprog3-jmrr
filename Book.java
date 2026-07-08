b7c562704eba789eb807a0df71762c70f7931017
jimlorm <jimlormmartinez@gmail.com>
2026-07-08 19:53:19 +0800

    feat: initialize `Book` class

--- Book.java
+++ Book.java
@@ -1,3 +1,14 @@
 public class Book extends MediaEntry {
-    
+    private String author;
+    private int pageCount;
+
+    public Book(String title, String genre, String status, String author, int pageCount) {
+        super(title, genre, status);
+        this.author = author;
+        this.pageCount = pageCount;
+    }
+
+    public void displayDetails() {
+
+    }
 }
