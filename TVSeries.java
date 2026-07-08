daafb9349de77a3e399f9518ca6c99a0eb156c97
jimlorm <jimlormmartinez@gmail.com>
2026-07-08 19:55:12 +0800

    feat: initialize `TVSeries` class

--- TVSeries.java
+++ TVSeries.java
@@ -1,3 +1,20 @@
+import java.util.ArrayList;
+
 public class TVSeries extends MediaEntry {
-    
+    private int totalEpisodes;
+    private ArrayList<Episode> episodes;
+
+    public TVSeries(String title, String genre, String status, int totalEpisodes, ArrayList<Episode> episodes) {
+        super(title, genre, status);
+        this.totalEpisodes = totalEpisodes;
+        this.episodes = new ArrayList<>(episodes);
+    }
+
+    public void addEpisode(Episode ep) {
+        episodes.add(ep);
+    }
+
+    public void displayDetails() {
+
+    }
 }
