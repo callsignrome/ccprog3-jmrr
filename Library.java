5ecf66a16f29ef071f435ecd257f271a756671bb
Raphael Romero <raphael.romero05@aol.com>
2026-07-09 00:58:03 +0800

    fix: changed methods to use new static final statuses

--- Library.java
+++ Library.java
@@ -12,7 +12,7 @@ public class Library {
     }
 
     public void rateEntry(MediaEntry m, int rating, String review) {
-        if (m.getCurrentStatus().equals("Completed") == true) {
+        if (m.getCurrentStatus().equals(MediaEntry.STATUSES[2])) {
             // input validation for out of bounds rating
             while (rating > 5 || rating < 0) {
                 System.out.println("Error: Ratings are only from 0 - 5 stars! Try again.");
@@ -32,28 +32,28 @@ public class Library {
     public void updateProgress(MediaEntry m) {
         System.out.println(m.getTitle() + ": " + m.getCurrentStatus());
         System.out.println("Update status to:");
-        System.out.println("[1] Planned");
-        System.out.println("[2] In Progress");
-        System.out.println("[3] Completed");
+        for (int i = 0; i < MediaEntry.STATUSES.length; i++)
+            System.out.println("["+ i +"] " + MediaEntry.STATUSES[i]);
         System.out.println("Enter: ");
+
         Scanner sc2 = new Scanner(System.in);
         int status = sc2.nextInt();
 
         while (status > 3 || status < 1) {
             System.out.println("Error: Input out of bounds. Try again: ");
-            sc2.nextInt();
+            status = sc2.nextInt();
             sc2.close();
         }
 
         switch (status) {
             case 1:
-                m.setCurrentStatus("Planned");
+                m.setCurrentStatus(MediaEntry.STATUSES[0]);
                 break;
             case 2:
-                m.setCurrentStatus("In Progress");
+                m.setCurrentStatus(MediaEntry.STATUSES[1]);
                 break;
             case 3:
-                m.setCurrentStatus("Completed");
+                m.setCurrentStatus(MediaEntry.STATUSES[2]);
                 break;
         }
     }
