aa677f756eeef9744123e625cb342d80d167c6c9
jimlorm <jimlormmartinez@gmail.com>
2026-07-09 16:53:51 +0800

    fix: add constructor

--- Episode.java
+++ Episode.java
@@ -3,6 +3,12 @@ public class Episode {
     private int episodeNumber;
     private int season;
 
+    public Episode(String title, int episodeNumber, int season) {
+        this.title = title;
+        this.episodeNumber = episodeNumber;
+        this.season = season;
+    }
+    
     public void displayDetails() {
         
     }
