e0410f2fc94d58eda6988b4d6ffcd4a343315588
jimlorm <jimlormmartinez@gmail.com>
2026-07-10 16:48:41 +0800

    refactor: add getters and setters

--- Episode.java
+++ Episode.java
@@ -8,6 +8,30 @@ public class Episode {
         this.episodeNumber = episodeNumber;
         this.season = season;
     }
+
+    public String getTitle() { 
+        return title; 
+    }
+
+    public void setTitle(String title) { 
+        this.title = title; 
+    }
+
+    public int getEpisodeNumber() { 
+        return episodeNumber; 
+    }
+
+    public void setEpisodeNumber(int episodeNumber) { 
+        this.episodeNumber = episodeNumber; 
+    }
+
+    public int getSeason() { 
+        return season; 
+    }
+
+    public void setSeason(int season) { 
+        this.season = season; 
+    }
     
     public void displayDetails() {
         System.out.println("Season " + season + " Ep " + episodeNumber + ": " + title);
