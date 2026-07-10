0d0cac1f13cb285d0f2d469a656f057599821554
jimlorm <jimlormmartinez@gmail.com>
2026-07-10 16:41:24 +0800

    refactor: add getters and setters

--- Book.java
+++ Book.java
@@ -8,6 +8,22 @@ public class Book extends MediaEntry {
         this.pageCount = pageCount;
     }
 
+    public String getAuthor() { 
+        return author; 
+    }
+
+    public void setAuthor(String author) {
+        this.author = author; 
+    }
+
+    public int getPageCount() { 
+        return pageCount; 
+    }
+
+    public void setPageCount(int pageCount) {
+        this.pageCount = pageCount; 
+    }
+
     public void displayDetails() {
         System.out.println("[Book] " + getTitle());
         System.out.println("Author: " + author);
